2d22d9a759267e512ff7e8df5b26d5d8f7f774af
Asek3 <64613894+Asek3@users.noreply.github.com>
2021-04-13 15:39:03 +0300

    Remaked Battery System

--- src/main/java/beyondspace/items/PortableBattery.java
+++ src/main/java/beyondspace/items/PortableBattery.java
@@ -57,13 +57,16 @@ public class PortableBattery extends ItemElectricBase {
 	}
 	
     @Override
+    public boolean hasEffect(ItemStack stack, int pass) {
+        return getShort(stack, "Mode", (short) 0) > 0;
+    }
+	
+    @Override
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
     	NBTTagCompound compound = getCompound(stack);
         if (player.isSneaking()) {
             int mode = getShort(stack, "Mode", (short) 0);
             setShort(stack, "Mode", (short) ((short) mode == 1 ? mode = 0 : mode+1));
-            if (!world.isRemote)
-                player.addChatComponentMessage(new ChatComponentTranslation(StatCollector.translateToLocal("beyondspace.charger") + ": " + StatCollector.translateToLocal("beyondspace.charger.mode") + mode));
         }
         return stack;
     }
