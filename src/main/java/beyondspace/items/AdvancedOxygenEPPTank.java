4060083d8e347404f3de5ca8fa5a18de2cd5c91c
Asek3 <64613894+Asek3@users.noreply.github.com>
2021-04-16 18:07:27 +0300

    Maked Advanced EPP Tank

--- src/main/java/beyondspace/items/AdvancedOxygenEPPTank.java
+++ src/main/java/beyondspace/items/AdvancedOxygenEPPTank.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 import beyondspace.BeyondSpace;
 import beyondspace.ModInfo;
+import galaxyspace.systems.SolarSystem.planets.overworld.items.ItemGSOxygenTank;
 import micdoodle8.mods.galacticraft.core.items.ItemOxygenTank;
 import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-public class AdvancedOxygenTank extends ItemOxygenTank
+public class AdvancedOxygenEPPTank extends ItemGSOxygenTank
 	{
-    public AdvancedOxygenTank(int tier, String assetName)
+	
+	private static boolean epp;
+	  
+	private int tier;
+	
+    public AdvancedOxygenEPPTank(int tier, String assetName, boolean epp)
     {
-        super(tier, assetName);
+        super(tier, assetName, tier, epp);
         this.setMaxStackSize(1);
         this.setMaxDamage(tier * 1080);
         this.setUnlocalizedName(assetName);
@@ -26,7 +32,7 @@ public class AdvancedOxygenTank extends ItemOxygenTank
     @Override
     public CreativeTabs getCreativeTab()
     {
-        return BeyondSpace.gaTab;
+        return BeyondSpace.bsTab;
     }
     
     @SuppressWarnings({ "unchecked", "rawtypes" })
@@ -34,5 +40,13 @@ public class AdvancedOxygenTank extends ItemOxygenTank
     public void addInformation(ItemStack par1ItemStack, EntityPlayer player, List par2List, boolean b)
     {
         par2List.add(GCCoreUtil.translate("gui.tank.oxygenRemaining") + ": " + (par1ItemStack.getMaxDamage() - par1ItemStack.getItemDamage()));
+    }
+
+    public int getTier() {
+        return 3;
+    }
+      
+      public boolean getEPP() {
+        return true;
     }
 }
