174a9b144794e81e0ce87d8fa75d39ef49a46b4d
Asek3 <bogban.zp@gmail.com>
2021-07-12 20:01:22 +0300

    Updated Version

--- src/main/java/beyondspace/ModInfo.java
+++ src/main/java/beyondspace/ModInfo.java
@@ -5,5 +5,5 @@ public class ModInfo {
 	public static final String MODID = "beyondspace";
 	public static final String MIDP = MODID + ':';
 	public static final String NAME = "Beyond Space";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 }
